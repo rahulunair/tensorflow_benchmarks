4a011aac75e19d8a0866e45c826185342e77b8d5
runnikri <rahul.unnikrishnan.nair@intel.com>
2019-05-23 18:55:35 -0700

    adding main with conda envs

--- main.sh
+++ main.sh
@@ -1,5 +1,21 @@
 #! /usr/bin/bash
 
+echo "creating a conda env with non mkl TensorFlow"
+conda env create --file=./no_mkl_environment.yml -q &&\
+  conda activate tf-no_mkl
+# run benchmark and save data
+./bench_tf.sh
+conda deactivate &&\
+  conda env remove tf-no_mkl
+echo "collected data for non mkl version"
+echo "creating a conda env with mkl based TensorFlow"
+conda env create --file=./mkl_environment.yml -q &&\
+  conda activate tf-mkl
+# run benchmark and save data
+./bench_tf.sh
+conda deactivate &&\
+  conda env remove tf-mkl
+echo "collected data for mkl version"
 # run benchmark and save data
 ./bench_tf.sh
 # minimal etl
